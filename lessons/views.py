9a3575cbd3f15b15a50929a5b353588d6940c8f7
Eugen Ciur <eugen@django-lessons.com>
2019-12-14 08:01:51 +0100

    Retrieve all lessons

--- lessons/views.py
+++ lessons/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from lessons.models import Lesson
 
 
 def index(request):
-    return render(request, 'lessons/index.html')
+    lessons = Lesson.objects.all().order_by('-updated_at')
+
+    return render(
+        request,
+        'lessons/index.html',
+        {'lessons': lessons}
+    )
