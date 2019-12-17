79d01eaa7520fdb6171f96255111b7c491d2be68
Eugen Ciur <eugen@django-lessons.com>
2019-12-17 08:43:53 +0100

    basic testing

--- lessons/tests.py
+++ lessons/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class LessonsViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        resp = self.client.get(reverse('index'))
+
+        self.assertEqual(
+            resp.status_code, 200
+        )
+
+
+class PageViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_about(self):
+        resp = self.client.get(reverse('about'))
+
+        self.assertEqual(
+            resp.status_code, 200
+        )
+
+    def test_privacy(self):
+        resp = self.client.get(reverse('privacy'))
+
+        self.assertEqual(
+            resp.status_code, 200
+        )
+
+    def test_impressum(self):
+        resp = self.client.get(reverse('impressum'))
+
+        self.assertEqual(
+            resp.status_code, 200
+        )
+
