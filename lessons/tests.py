05d76dee3ee762918e93d90bba32c0afaec1e1db
Eugen Ciur <eugen@django-lessons.com>
2019-12-17 08:58:25 +0100

    and now with proper testing

--- lessons/tests.py
+++ lessons/tests.py
@@ -1,12 +1,29 @@
+from django.utils import timezone
 from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
 
 
+from lessons.models import (Lesson, User)
+
+
 class LessonsViewTests(TestCase):
 
     def setUp(self):
+        self.user = User.objects.create(
+            username="user1",
+        )
         self.client = Client()
+        self.lesson_1 = Lesson.objects.create(
+            title="Lesson1 ",
+            order=1,
+            description="Very interesting information about python3.6",
+            created_at=timezone.now(),
+            updated_at=timezone.now(),
+            publish_date=timezone.now(),
+            published=True,
+            user=self.user
+        )
 
     def test_index(self):
         resp = self.client.get(reverse('index'))
