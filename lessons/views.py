e7a052cf6c3887bf8c53c569c1cb3ad33db194d9
Eugen Ciur <eugen@django-lessons.com>
2020-02-05 07:37:15 +0100

    index and lesson views restored

--- lessons/views.py
+++ lessons/views.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 
 from lessons.models import Subscribtion
 from lessons.forms import SubscribeForm
+from lessons.models import Lesson
 
 
 logger = logging.getLogger(__name__)
@@ -12,6 +13,37 @@ logger = logging.getLogger(__name__)
 
 def handler500(request):
     return render(request, "lessons/500.html")
+
+
+def index(request):
+    """
+        Landing page will list published lessons
+        ordered by update_at (DESC)
+    """
+    if request.method != 'GET':
+        return HttpResponseBadRequest()
+
+    lessons = Lesson.objects.all()
+
+    return render(
+        request,
+        'lessons/index.html',
+        {'lessons': lessons}
+    )
+
+
+def lesson(request, order, slug):
+    try:
+        lesson = Lesson.objects.get(order=order)
+    except Lesson.DoesNotExist:
+        logger.warning(f"Lesson #{order} not found")
+        raise Http404("Lesson not found")
+
+    return render(
+        request,
+        'lessons/lesson.html',
+        {'page': lesson}
+    )
 
 
 class PageView(TemplateView):
