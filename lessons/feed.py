d91e8a622d5963cb0c7dec7f8719a70d18408185
Eugen Ciur <eugen@django-lessons.com>
2020-02-06 10:16:09 +0100

    oops, I forgot to updated RSS

--- lessons/feed.py
+++ lessons/feed.py
@@ -8,10 +8,10 @@ class LatestLessonsFeed(Feed):
     description = "Keep track of latest Lessons."
 
     def items(self):
-        return Lesson.objects.order_by('-created_at')[:5]
+        return Lesson.objects.order_by('-first_published_at')[:5]
 
     def item_title(self, item):
         return item.title
 
     def item_description(self, item):
-        return item.description
+        return item.short_description
