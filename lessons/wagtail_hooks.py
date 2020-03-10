f93c65894d705604faf2dfd8d6bad4c107410b6f
Eugen Ciur <eugen@django-lessons.com>
2020-03-10 08:14:05 +0100

    nice cms admin for course/lesson management

--- lessons/wagtail_hooks.py
+++ lessons/wagtail_hooks.py
@@ -1,6 +1,6 @@
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, modeladmin_register)
-from .models import (Lesson, LessonGroup)
+from .models import (Lesson, LessonGroup, Course)
 
 
 class LessonAdmin(ModelAdmin):
@@ -16,8 +16,35 @@ class LessonAdmin(ModelAdmin):
 class LessonGroupAdmin(ModelAdmin):
     model = LessonGroup
 
+    list_display = (
+        'order',
+        'get_lesson',
+        'get_course',
+    )
+
+    def get_course(self, obj):
+        return obj.course
+
+    def get_lesson(self, obj):
+        return obj.lesson
+
+    get_course.short_description = 'Course'
+    get_course.admin_order_field = 'course__title'
+
+    get_lesson.short_description = 'Lesson'
+    get_lesson.admin_order_field = 'lesson__title'
+
+
+class CourseAdmin(ModelAdmin):
+    model = Course
+    list_display = (
+        'title',
+        'live'
+    )
+
 
 # Now you just need to register your customised ModelAdmin class with Wagtail
 modeladmin_register(LessonAdmin)
 modeladmin_register(LessonGroupAdmin)
+modeladmin_register(CourseAdmin)
 
