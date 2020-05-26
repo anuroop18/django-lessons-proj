46ae59533b73d26a558a499551653fd5460fed16
Eugen Ciur <eugen@django-lessons.com>
2020-05-26 07:24:25 +0200

    more user friendly messages for 3Ds payments

--- lessons/static/lessons/js/3dsec.js
+++ lessons/static/lessons/js/3dsec.js
@@ -5,11 +5,15 @@ function _3dsec(stripe_publishable_key, pi_secret) {
       stripe.confirmCardPayment(pi_secret).then(function(result) {
         if (result.error) {
           // Display error.message in your UI.
-          $("#3ds_result").text("Error!");
+          $("#3ds_result").text(
+            "Error occured during payment process. Please contact me via email: eugen@django-lessons.com, I will assist you."
+          );
           $("#3ds_result").addClass("text-danger");
         } else {
           // The payment has succeeded. Display a success message.
-          $("#3ds_result").text("Thank you for payment");
+          $("#3ds_result").text(
+            "Thank you! It may take 2-3 minutes to process the payment and update your account."
+          );
           $("#3ds_result").addClass("text-success");
         }
       });
