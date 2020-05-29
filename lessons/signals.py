d9c5d8d2dab0f532d1d3f292e766820cb2e1d3ed
Eugen Ciur <eugen@django-lessons.com>
2020-05-29 07:12:40 +0200

    define signals for important events

--- lessons/signals.py
+++ lessons/signals.py
@@ -1,10 +1,14 @@
-from django.dispatch import receiver
-from django.db.models.signals import (
-    post_save,
-)
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import Signal, receiver
 
 from .models import UserProfile
+
+upgrade_open = Signal()
+upgrade_in_progress = Signal()
+upgrade_complete_error = Signal()
+upgrade_complete_success = Signal()
+pro_screencast_view_open = Signal()
 
 
 @receiver(post_save, sender=User)
@@ -14,3 +18,19 @@ def update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(
             user=instance
         )
+
+
+def upgrade_open_handler(sender, **kwargs):
+    pass
+
+
+def upgrade_in_progress_handler(sender, **kwargs):
+    pass
+
+
+def upgrade_complete_error_handler(sender, **kwargs):
+    pass
+
+
+def upgrade_complete_success_handler(sender, **kwargs):
+    pass
