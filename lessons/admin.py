d4c5f1804bc4cf84e88f4443d81321e22f56cd33
Eugen Ciur <eugen@django-lessons.com>
2020-05-30 07:32:15 +0200

    IsPROList filter included

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,7 +1,60 @@
+from datetime import date
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 from .models import Contact, Course, Lesson, LessonGroup, Subscription
+
+
+class IsPROListFilter(admin.SimpleListFilter):
+    title = _('is PRO')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'pro'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return (
+            ('yes', _('Yes')),
+            ('no', _('No')),
+        )
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        _today = date.today()
+        if self.value() == 'yes':
+            return queryset.filter(profile__pro_enddate__gte=_today)
+        if self.value() == 'no':
+            return queryset.filter(profile__pro_enddate__lte=_today)
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        'username',
+        'email',
+        'is_pro',
+        'is_staff',
+        'first_name',
+        'last_name',
+    )
+    list_filter = UserAdmin.list_filter + (IsPROListFilter,)
+
+    def is_pro(self, obj):
+        return obj.profile.is_pro_user()
+
+    is_pro.boolean = True
 
 
 class ContactAdmin(admin.ModelAdmin):
@@ -24,6 +77,8 @@ class LessonGroupAdmin(admin.ModelAdmin):
     pass
 
 
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Subscription, SubscriptionAdmin)
 admin.site.register(Contact, ContactAdmin)
 admin.site.register(Course, CourseAdmin)
